2b293c43a2178d3fdbf44ec666b04aac046327da
Yohanes Dwi Pratama <yhndp09@gmail.com>
2025-06-05 15:50:19 +0700

    Update run_job.bat

--- jenkins/run_job.bat
+++ jenkins/run_job.bat
@@ -1,6 +1,8 @@
 @echo off
+
+REM Pentaho Kitchen location
 set PDI_HOME=C:\Users\yohanes.pratama\Desktop\PENTAHO
 set JOB_FILE=%CD%\JOB\Job_test.kjb
 
 cd %PDI_HOME%
-call Kitchen.bat /file:%JOB_FILE% /level:Basic > %CD%\jenkins\job_log.txt 2>&1
+call Kitchen.bat /file:%JOB_FILE% /level:Basic
