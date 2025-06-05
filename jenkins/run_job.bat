461df38f2fb0e3830b1c5c9265e17e097c900cb6
Yohanes Dwi Pratama <yhndp09@gmail.com>
2025-06-05 15:44:06 +0700

    Update run_job.bat

--- jenkins/run_job.bat
+++ jenkins/run_job.bat
@@ -1,11 +1,6 @@
 @echo off
-
-REM Lokasi folder Kitchen Pentaho di lokal
 set PDI_HOME=C:\Users\yohanes.pratama\Desktop\PENTAHO
-
-REM Jalankan job dari hasil clone GitHub (Jenkins workspace)
 set JOB_FILE=%CD%\JOB\Job_test.kjb
 
-echo Running job: %JOB_FILE%
 cd %PDI_HOME%
-call Kitchen.bat /file:%JOB_FILE% /level:Basic
+call Kitchen.bat /file:%JOB_FILE% /level:Basic > %CD%\jenkins\job_log.txt 2>&1
