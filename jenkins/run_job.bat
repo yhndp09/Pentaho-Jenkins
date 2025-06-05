46c4962a61bdc0b1ed0c040d6a0717da4cd4fed6
Yohanes Dwi Pratama <yhndp09@gmail.com>
2025-06-05 17:05:30 +0700

    Update run_job.bat

--- jenkins/run_job.bat
+++ jenkins/run_job.bat
@@ -1,21 +1,12 @@
 @echo off
-echo === START run_job.bat ===
+echo === RUNNING JOB ===
+
+REM Pentaho Kitchen location
 set PDI_HOME=C:\Users\yohanes.pratama\Desktop\PENTAHO
-set JOB_FILE=C:\ProgramData\Jenkins\.jenkins\workspace\pentaho-pipeline\JOB\Job_test.kjb
+set JOB_FILE=%CD%\JOB\Job_test.kjb
 
-echo PDI_HOME=%PDI_HOME%
-echo JOB_FILE=%JOB_FILE%
+cd /d %PDI_HOME%
+echo Current DIR: %CD%
+echo Running job file: %JOB_FILE%
 
-cd /d %PDI_HOME% || (
-    echo FAILED TO CD TO PDI_HOME
-    exit /b 1
-)
-
-if not exist Kitchen.bat (
-    echo Kitchen.bat NOT FOUND
-    dir
-    exit /b 1
-)
-
-echo Starting Kitchen.bat
 call Kitchen.bat /file:%JOB_FILE% /level:Basic
