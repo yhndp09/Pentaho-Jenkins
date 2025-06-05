9e6cf481d92843daee0c6fd939fd3a879efd7b64
Yohanes Dwi Pratama <yhndp09@gmail.com>
2025-06-05 16:23:33 +0700

    Update run_job_dan_tranformation.bat

--- jenkins/run_job_dan_tranformation.bat
+++ jenkins/run_job_dan_tranformation.bat
@@ -1,7 +1,6 @@
 @echo off
-REM Path ke Pentaho dan transformasi
 set PDI_HOME=C:\Users\yohanes.pratama\Desktop\PENTAHO
-set TRANS_FILE=C:\ProgramData\Jenkins\.jenkins\workspace\pentaho-pipeline\Transformation\get_data.ktr
+set JOB_FILE=C:\ProgramData\Jenkins\.jenkins\workspace\pentaho-pipeline\JOB\Job_test.kjb
 
 cd %PDI_HOME%
-call pan.bat /file:%TRANS_FILE% /level:Basic
+call Kitchen.bat /file:%JOB_FILE% /level:Basic
