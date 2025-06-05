adbf84097c3a27642edcf50ebf0497588cbf4d45
Yohanes Dwi Pratama <yhndp09@gmail.com>
2025-06-05 16:24:02 +0700

    Create run_job.bat

---
+++ jenkins/run_job.bat
@@ -0,0 +1,6 @@
+@echo off
+set PDI_HOME=C:\Users\yohanes.pratama\Desktop\PENTAHO
+set JOB_FILE=C:\ProgramData\Jenkins\.jenkins\workspace\pentaho-pipeline\JOB\Job_test.kjb
+
+cd %PDI_HOME%
+call Kitchen.bat /file:%JOB_FILE% /level:Basic
