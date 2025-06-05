81f0602a313c2873db974f0ac77cc0a0cfe6e5a7
Yohanes Dwi Pratama <yhndp09@gmail.com>
2025-06-05 17:00:02 +0700

    Update run_job.bat

--- jenkins/run_job.bat
+++ jenkins/run_job.bat
@@ -2,5 +2,5 @@
 set PDI_HOME=C:\Users\yohanes.pratama\Desktop\PENTAHO
 set JOB_FILE=C:\ProgramData\Jenkins\.jenkins\workspace\pentaho-pipeline\JOB\Job_test.kjb
 
-cd %PDI_HOME%
+cd /d %PDI_HOME%
 call Kitchen.bat /file:%JOB_FILE% /level:Basic
