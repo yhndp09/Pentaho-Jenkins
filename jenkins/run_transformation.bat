cbcb153156a07537d5f88b2f50354ca0d02b352f
Yohanes Dwi Pratama <yhndp09@gmail.com>
2025-06-09 17:01:01 +0700

    Delete jenkins/run_transformation.bat

--- jenkins/run_transformation.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM Path ke Pentaho dan transformasi
-set PDI_HOME=C:\Users\yohanes.pratama\Desktop\PENTAHO
-set TRANS_FILE=C:\ProgramData\Jenkins\.jenkins\workspace\pentaho-pipeline\Transformation\get_data.ktr
-
-cd %PDI_HOME%
-call pan.bat /file:%TRANS_FILE% /level:Basic
